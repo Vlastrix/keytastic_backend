ff4d80c45686022461df0c41812e152252b8dad2
Vladislav Bochkov <vlad.bochkov.ch@gmail.com>
2023-01-19 17:43:56 -0400

    added mongoose and MongoDB connection

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
 const port = process.env.PORT || 3000
 
-app.get("/", function(req, res) {
+// Database connection
+const mongoDBUrl = process.env.MONGODBURL;
+mongoose.connect(mongoDBUrl);
+
+// Items Schema
+const itemsSchema = new mongoose.Schema({
+    name: String,
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your To Do List!"
+});
+
+app.get("/singin", function(req, res) {
+    res.send("Halo");
+});
+
+app.get("/singup", function(req, res) {
     res.send("Halo");
 });
 
