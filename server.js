0fc52acd3b0cdc803528cd77478613c7422a34ea
Vladislav Bochkov <vlad.bochkov.ch@gmail.com>
2023-02-01 20:11:13 -0400

    fix semicolon in server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = process.env.PORT || 3000;
