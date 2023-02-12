f7bf812e9b6cda1eddf05eb08fb585925b93d165
Vladislav Bochkov <vlad.bochkov.ch@gmail.com>
2023-02-12 17:10:29 -0400

    refactor userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,14 +14,6 @@ function checkForError(err, res) {
     return false;
 }
 
-function checkIfAlreadyExists(foundUser, res) {
-    if (foundUser != null) {
-        res.status(400).send("This email is already registered. Please enter another email.");
-        return true;
-    }
-    return false;
-}
-
 function createUser(username, email, hashedPassword, res) {
     const newUser = new User({
         username: username,
@@ -41,11 +33,11 @@ const signUp = (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     bcrypt.hash(password, saltRounds, function(bcryptError, hashedPassword) {
-        if (checkForError(bcryptError, res)) return res.end()
-        User.exists({email: req.body.email}, function (searchError, foundUser) {
-            if (checkForError(searchError, res)) return res.end()
-            if (checkIfAlreadyExists(foundUser, res)) return res.end();
-            createUser(username, email, hashedPassword ,res);
+        if (checkForError(bcryptError, res)) return res.end();
+        User.exists({email: email}, function (searchError, foundUser) {
+            if (checkForError(searchError, res)) return res.end();
+            if (foundUser != null) return res.status(400).send("This email is already registered. Please enter another email.");
+            createUser(username, email, hashedPassword, res);
         });
     });
 }
@@ -53,41 +45,26 @@ const signUp = (req, res) => {
 const signIn = (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    User.findOne({email: email}, function(mongooseError, foundUser){
-        if (checkForError(mongooseError, res)) return res.end()
-        if (mongooseError) {
-            console.log(mongooseError);
-        } else if (foundUser === null) {
-            res.status(404).send("This user does not exist. Please Sign Up.");
-        } else if (foundUser.isActive === false) {
-            res.status(403).send("This user is innactive.");
-        } else {
-            bcrypt.compare(password, foundUser.password, function(bcryptError, result) {
-                if (bcryptError) {
-                    console.log(bcryptError);
-                } else if (result === true) {
-                    var jsonWebToken = jwt.sign({ user: foundUser._id }, process.env.JWT_SECRET, {expiresIn: '24h'});
-                    res.status(200).json({token: jsonWebToken});
-                } else {
-                    res.status(400).send("Email or Password is incorrect.");
-                }
-            });
-        }
+    User.findOne({email: email}, function(mongooseError, foundUser) {
+        if (checkForError(mongooseError, res)) return res.end();
+        if (foundUser === null) return res.status(404).send("This user does not exist. Please Sign Up.");
+        if (foundUser.isActive === false) return res.status(403).send("This user is innactive.");
+        bcrypt.compare(password, foundUser.password, function(bcryptError, result) {
+            if (checkForError(bcryptError, res)) return res.end();
+            if (!result) return res.status(400).send("Email or Password is incorrect.");
+            var jsonWebToken = jwt.sign({ user: foundUser._id }, process.env.JWT_SECRET, {expiresIn: '24h'});
+            res.status(200).json({token: jsonWebToken});
+        });
     });
 }
 
 const verifyToken = (req, res) => {
     const receivedToken = req.body.token;
     jwt.verify(receivedToken, process.env.JWT_SECRET, function(jsonWebTokenError, decoded) {
-        if (jsonWebTokenError) {
-            console.log(jsonWebTokenError);
-            if (jsonWebTokenError.name === "TokenExpiredError") {
-                res.status(200).send("Token expired.");
-            }
-            // Add something went wrong
-        } else {
-            res.status(200).send("Token validated!");
-        }
+        if (jsonWebTokenError && jsonWebTokenError.name != "TokenExpiredError") return res.end();
+        if (jsonWebTokenError.name === "TokenExpiredError") return res.status(200).send("Token expired.");
+        res.status(200).send("Token validated!");
+
     });
 }
 
